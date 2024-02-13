3cc38ba51096ebe33ec0b8b918ff22ef463bd898
Ihafnonaim <varshinijonnala1@gmail.com>
2024-02-13 14:19:41 +0530

    .

---
+++ microctrl/class_names.py
@@ -0,0 +1,15 @@
+INSTANCE_CATEGORY_NAMES = [
+    '__background__', 
+    'bicycle',
+    'bike',
+    'bus',
+    'car',
+    'obstacle',
+    'pedestrian',
+    'rider',
+    'scooter',
+    'traffic light',
+    'tram',
+    'truck',
+    'van'
+]
